7d065fffad46c5f6f017e7f86d92d1f3fa2c27e5
ychogriz <ychogriz@gmail.com>
2014-11-03 11:24:38 +0100

    Added tests

---
+++ TestOne/src/main/java/Entities/Person.java
@@ -0,0 +1,23 @@
+package Entities;
+
+public class Person {
+
+	private String name;
+	private String lastName;
+	
+	public Person() {
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+}
---
+++ TestOne/src/test/java/TestEntities/TestPerson.java
@@ -0,0 +1,16 @@
+package TestEntities;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import Entities.Person;
+
+public class TestPerson {
+
+	@Test
+	public void testPerson() {
+		Person p = new Person();
+		p.setLastName("Chogriz");
+		assertEquals(p.getLastName(), "Chogriz");
+	}
+}
